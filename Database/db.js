b62b39b7f6527a2ac3e9dca1d7c2707a1fa8bc3e
Muhammad Atif <muhammadatif6541@gmail.com>
2025-08-25 12:55:39 +0500

    Database set

--- Database/db.js
+++ Database/db.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 dotenv.config();
 export const connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI,{
+      dbName: 'todo-db'
+    });
     console.log('MongoDB connected');
   } catch (err) {
     console.error('Error while connecting to MongoDB:', err);
