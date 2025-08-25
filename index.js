21c5ed3e3b78db5337df0909e19461bf50248e41
Muhammad Atif <muhammadatif6541@gmail.com>
2025-08-25 12:42:31 +0500

    Atlas configured again

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+require('dotenv').config
 import express from 'express';
 import cors from 'cors';
 import router from './routes/router.js';
 
 const app = express();
+
 app.use(cors({
-    origin: 'https://todo-list-fro.netlify.app',
+    origin: ['https://todo-list-fro.netlify.app'],
     methods: ['GET', 'DELETE', 'PUT', 'POST'],
-    credentials: true
 }));
+
 app.use(express.json());
 
 app.use(router);
 
-app.listen(3000, () => console.log('running'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
