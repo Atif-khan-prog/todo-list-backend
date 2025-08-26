a86df9ae3d04dc32b24585db2c1ed0099ef03a5b
Muhammad Atif <muhammadatif6541@gmail.com>
2025-08-26 07:19:08 +0500

    Database fixed again

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv'
+dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import router from './routes/router.js';
-dotenv.config();
+
 const app = express();
 
 app.use(cors({
